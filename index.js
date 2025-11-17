e92854a9554e8259dfc3e986a3506e5950ac2eab
Anna Ek <anna.ek2@outlook.com>
2025-11-17 17:35:57 +0100

    Annas WJ's del Refactor recipe display and loading functionality; enhance styling and structure

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 //Recept
+
+async function getRecipes() {
+    try {
+        const res = await fetch('https://dummyjson.com/recipes')
+        const data = await res.json()
+        renderRecipes(data.recipes)
+    } catch (error) {
+        console.error('Kunde inte hämta recept:', error)
+    }
+}
+
+function renderRecipes(recipes) {
+    const container = document.querySelector('#mainSection')
+    container.innerHTML = ''
+    recipes.forEach((recipe) => {
+        console.log(recipe)
+        const card = document.createElement('div')
+        card.className = 'recipe'
+        card.innerHTML = `
+        <img
+            src="${recipe.image}"
+            alt="test"
+        />
+        <div class="content">
+            <h2>${recipe.name}</h2>
+            <p>Preparation time: <b>${recipe.prepTimeMinutes} min</b></p>
+            <p>Cooking time: <b>${recipe.cookTimeMinutes} min</b></p>
+            <div class="btn-container">
+                <a class="viewRecipe" href="#">View recept</a>
+                <button>Delete</button>
+            </div>
+        </div>
+      `
+        container.appendChild(card)
+    })
+}
+
+getRecipes()
